0d98b42daaa0eab97193c05d82faf1644e2c483e
Mauriverti da Silva Junior <maurivertijr@gmail.com>
2024-01-21 23:48:33 -0300

    Fix lint

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, redirect } from 'react-router-dom';
+import { createBrowserRouter, redirect } from 'react-router-dom'
 import UserCreate from './user/view/pages/UserCreate'
 import Login from './login/view/pages/Login'
 
