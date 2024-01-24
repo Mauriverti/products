4550b193a5f87ac9851106c84bf8a9a450fef6c9
Mauriverti da Silva Junior <maurivertijr@gmail.com>
2024-01-24 17:24:52 -0300

    Remove console

--- src/shared/domain/useStore.hook.ts
+++ src/shared/domain/useStore.hook.ts
@@ -6,7 +6,6 @@ export default function useLocalStorage<T>(key: string, initialValue?: T) {
       const item = localStorage.getItem(key)
       return item ? JSON.parse(item) : initialValue
     } catch (e) {
-      console.error(e)
       return initialValue
     }
   })
